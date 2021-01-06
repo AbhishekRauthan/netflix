e850444288074bc28c36a66ea259e329282f9079
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2021-01-06 19:32:55 +0530

    done with nav

--- layout/Header.js
+++ layout/Header.js
@@ -5,10 +5,12 @@ const Header = () => {
   return (
     <div className="header">
       <div className="nav">
-        <Image className="" src="/img/logo.png" height={100} width={195} />
+        <div className="logo">
+          <Image src="/img/logo.png" height={100} width={195} />
+        </div>
 
         <Link href="/signup" >
-          <a className="btn btn-red">Sign Up</a>
+          <a className="btn btn-red btn-nav">Sign Up</a>
         </Link>
       </div>
     </div>
